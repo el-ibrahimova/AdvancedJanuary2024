44c1c81e3460cf956c14eac64ddebde6baae30b3
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-12-16 18:20:57 +0200

    Initial commit

---
+++ 01.StacksAndQueues-Lab/01.ReverseAString/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01.ReverseAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
