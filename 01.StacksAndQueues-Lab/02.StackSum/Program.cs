8aedc00f565c17b6d6ac2b74f78b734b49b7fccc
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-12-17 09:53:44 +0200

    create project

---
+++ 01.StacksAndQueues-Lab/02.StackSum/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _02.StackSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          //   List<int> input = Console.ReadLine().Split().Select(int.Parse).ToList();
+          // вместо да изписваме листа, директно го записваме new Stack<int> (..........);
+
+            Stack<int> stack = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+        }
+    }
+}
