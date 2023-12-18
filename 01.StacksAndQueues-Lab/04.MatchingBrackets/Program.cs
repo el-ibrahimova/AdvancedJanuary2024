15160e370ef53f9373bee84027163a54c8aa0240
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-12-18 20:07:39 +0200

    create project

---
+++ 01.StacksAndQueues-Lab/04.MatchingBrackets/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _04.MatchingBrackets
+{ 
+/*
+1 + (2 - (2 + 3) * 4 / (3 + 1)) * 5
+*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+
+            Stack<int> openingBrackets = new Stack<int>();
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '(')
+                {
+                    openingBrackets.Push(i);
+                }
+
+                if (expression[i] == ')')
+                {
+                    int openingBracketIndex = openingBrackets.Pop();
+
+                    Console.WriteLine(expression.Substring(openingBracketIndex, i-openingBracketIndex+1));
+                }
+            }
+        }
+    }
+}
