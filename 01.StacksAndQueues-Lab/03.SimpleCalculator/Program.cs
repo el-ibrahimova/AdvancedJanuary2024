0002141f6df02ae283ca1c4122abea37464d2f64
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-12-18 20:06:56 +0200

    create project

---
+++ 01.StacksAndQueues-Lab/03.SimpleCalculator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+namespace _03.SimpleCalculator
+{ 
+/*
+2 + 5 + 10 - 2 - 1
+*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            Stack< string> expression = new Stack<string>(input.Reverse());
+            // налага се да обърнем елементите, за да може да почнем да смятаме от първия елемент, т.е. от последно добавения в стека
+
+            int result = int.Parse(expression.Pop()); //2
+
+            while (expression.Any()) //докато в expression има някакви елементи
+            {
+                string sign = expression.Pop();
+                int number = int.Parse(expression.Pop());
+
+                if (sign == "+")
+                {
+                    result += number;
+                }
+                else // sign == "-"
+                {
+                    result -= number;
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
