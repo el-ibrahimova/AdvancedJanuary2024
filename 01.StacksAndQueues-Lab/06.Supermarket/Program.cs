2d4344f1fdb0a0385f4de3531a7601b329baef73
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-12-19 21:40:46 +0200

    create project

---
+++ 01.StacksAndQueues-Lab/06.Supermarket/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _06.Supermarket
+{ 
+/*
+Liam
+Noah
+James
+Paid
+Oliver
+Lucas
+Logan
+Tiana
+End
+*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            Queue<string> customers = new Queue<string>();
+            int counter = 0;
+
+            while (command != "End")
+            {
+                if (command == "Paid")
+                {
+                    while (customers.Count > 0)
+                    {
+                        Console.WriteLine(customers.Dequeue());
+                        counter = 0;
+                    }
+                }
+                else
+                {
+                    customers.Enqueue(command);
+                    counter++;
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"{counter} people remaining.");
+        }
+    }
+}
