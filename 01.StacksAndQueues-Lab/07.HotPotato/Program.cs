6fb70bffa5110a06d368a36ba7a8e4456c178986
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-12-19 21:41:08 +0200

    create project

---
+++ 01.StacksAndQueues-Lab/07.HotPotato/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.WebSockets;
+
+namespace _07.HotPotato
+{
+/*
+Alva James William
+2
+*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue< string> children = new Queue< string>(Console.ReadLine().Split());
+
+            int tossCount = int.Parse(Console.ReadLine());
+            int tosses = 0;
+
+            while (children.Count > 1)
+            {
+                tosses++;
+                string childWithPotato = children.Dequeue();
+                if (tosses == tossCount)
+                {
+                    tosses = 0; // започваме да броим от начало
+                    Console.WriteLine($"Removed {childWithPotato}");
+                }
+                else
+                {
+                    children.Enqueue(childWithPotato);
+                }
+            }
+
+            Console.WriteLine($"Last is {children.Dequeue()}");
+        }
+    }
+}
