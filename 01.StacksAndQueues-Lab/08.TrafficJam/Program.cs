0e00b5462e57e6d2e07afe5b2f61156c184e5c4d
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-12-20 21:02:07 +0200

    create project

---
+++ 01.StacksAndQueues-Lab/08.TrafficJam/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+namespace _08.TrafficJam
+{
+  /*
+4
+Hummer H2
+Audi
+Lada
+Tesla
+Renault
+Trabant
+Mercedes
+MAN Truck
+green
+green
+Tesla
+Renault
+Trabant
+end
+*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfPassedCars = int.Parse(Console.ReadLine());
+            Queue<string> cars = new Queue<string>();
+
+            int totalNumberOfCars = 0;
+
+            string command = Console.ReadLine();
+            while (command != "end")
+            {
+                if (command == "green")
+                {
+                    for (int i = 0; i < countOfPassedCars; i++)
+                    {
+                        if (cars.Count==0)
+                        {
+                            break;
+                        }
+
+                        Console.WriteLine($"{cars.Dequeue()} passed!"); ;
+                        totalNumberOfCars++;
+                    }
+                }
+                else
+                {
+                    cars.Enqueue(command);
+                }
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{totalNumberOfCars} cars passed the crossroads.");
+        }
+    }
+}
