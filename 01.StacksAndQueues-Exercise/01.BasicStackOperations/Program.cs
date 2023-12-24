95b90a97a5a66f6a0d81ed49bb61d8a5f4d0e23a
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-12-24 11:12:45 +0200

    update the code

--- 01.StacksAndQueues-Exercise/01.BasicStackOperations/Program.cs
+++ 01.StacksAndQueues-Exercise/01.BasicStackOperations/Program.cs
@@ -1,10 +1,47 @@
 ﻿namespace _01.BasicStackOperations
 {
+    /*
+    5 2 13
+    1 13 45 32 4
+    */
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] values = Console.ReadLine().Split().Select(int.Parse).ToArray();  // 5 2 13  == x, y, z
+
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();  // 1 13 45 32 4
+
+            int valuesToPush = values[0]; // 5
+            int valuesToPop = values[1];  // 2
+            int lookUpValue = values[2];  // 13
+
+            Stack<int> stack = new Stack<int>(input.Take(valuesToPush)); // кратък запис на следващия код
+
+         //   for (int i = 0; i < valuesToPush; i++)
+         // {
+         //   stack.Push(input[i]);
+         // }
+
+         
+            while (stack.Count > 0 && valuesToPop > 0)
+            {
+                stack.Pop();
+                valuesToPop--;
+            }
+
+            if (stack.Contains(lookUpValue))
+            {
+                Console.WriteLine("true");
+            }
+            else if (stack.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else // print smallest element
+            {
+                Console.WriteLine(stack.Min());
+            }
         }
     }
 }
