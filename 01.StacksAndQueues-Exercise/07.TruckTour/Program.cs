4a8dc7f18d7191d96d0012db73bd1729f7e47d3c
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-12-26 21:42:38 +0200

    create project

---
+++ 01.StacksAndQueues-Exercise/07.TruckTour/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace _07.TruckTour
+{
+/*
+3
+1 5
+10 3
+3 4
+*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int petrolPumpsCount = int.Parse(Console.ReadLine());
+
+            Queue<int[]> pumps = new Queue<int[]>();
+
+            for (int i = 0; i < petrolPumpsCount; i++)
+            {
+                int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();  // 1 5
+
+                int petrol = input[0];
+                int distance = input[1];
+
+                pumps.Enqueue(input);
+            }
+
+            int bestRoute = 0;
+
+            while (true)
+            {
+                int totalPetrol = 0;
+                
+                foreach (int[] pump in pumps)
+                {
+                   totalPetrol += pump[0]; //1 
+                    int currentDistance = pump[1]; //5
+
+                    if (totalPetrol - currentDistance < 0)
+                    {
+                        totalPetrol = 0;
+                        break;
+                    }
+                    else
+                    {
+                        totalPetrol -= currentDistance;
+                    }
+                }
+
+                if (totalPetrol >= 0)
+                {
+                    break;
+                }
+
+                bestRoute++;
+                pumps.Enqueue(pumps.Dequeue()); // премахва първата стойност 1 5 и я поставя най-отзад в опашката
+            }
+
+            Console.WriteLine(bestRoute);
+        }
+    }
+}
