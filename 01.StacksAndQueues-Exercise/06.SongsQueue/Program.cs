bdb66acfb92633093559a33c261da8d80e4521fa
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-12-26 21:42:23 +0200

    create project

---
+++ 01.StacksAndQueues-Exercise/06.SongsQueue/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace _06.SongsQueue
+{
+    /*
+    All Over Again, Watch Me
+    Play
+    Add Watch Me
+    Add Love Me Harder
+    Add Promises
+    Show
+    Play
+    Play
+    Play
+    Play
+    */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(", "); // All Over Again, Watch Me
+
+            Queue<string> songs = new Queue<string>(input);
+
+            while (true)
+            {
+                string[] commandInfo = Console.ReadLine().Split();
+
+                if (commandInfo[0] == "Play")
+                {
+                    if (songs.Any())
+                    {
+                        songs.Dequeue();
+                    }
+
+                    if (songs.Count == 0)
+                    {
+                        Console.WriteLine("No more songs!");
+                        break;
+                    }
+                }
+                else if (commandInfo[0] == "Add")
+                {
+                    // Add Watch Me 
+                    // ["Add", "Watch", "Me"]
+                    // ["Watch", "Me"]
+                    // string.Join -> Watch Me
+
+                    string songName = string.Join((" "), commandInfo.Skip(1));
+                    if (songs.Contains(songName))
+                    {
+                        Console.WriteLine($"{songName} is already contained!");
+                    }
+                    else
+                    {
+                        songs.Enqueue(songName);
+                    }
+                }
+                else if (commandInfo[0] == "Show")
+                {
+                    Console.WriteLine(string.Join(", ", songs));
+                }
+            }
+        }
+    }
+}
