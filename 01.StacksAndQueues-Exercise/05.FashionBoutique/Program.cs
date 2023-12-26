86a40701305a8c23e6b5318591e207129fa0c175
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-12-26 21:42:03 +0200

    create project

---
+++ 01.StacksAndQueues-Exercise/05.FashionBoutique/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _05.FashionBoutique
+{
+/*
+5 4 8 6 3 8 7 7 9
+16
+*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray(); // 5 4 8 6 3 8 7 7 9
+            int capacity = int.Parse(Console.ReadLine());
+
+            Stack<int> racks = new Stack<int>(input);
+
+            int totalUsedRacks = 1;
+            int currentCapacity = 0;
+
+            while (racks.Count > 0)
+            {
+                int currentValue = racks.Pop();
+
+                if (currentValue + currentCapacity <= capacity)
+                {
+                    currentCapacity += currentValue;
+                }
+                else
+                {
+                    totalUsedRacks++;
+                    currentCapacity = currentValue;
+                }
+            }
+
+            Console.WriteLine(totalUsedRacks);
+        }
+    }
+}
