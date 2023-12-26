54a63335e544ae3c9e5f782f2b08b08c40319225
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-12-26 21:41:44 +0200

    create project

---
+++ 01.StacksAndQueues-Exercise/04.FastFood/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _04.FastFood
+{ 
+/*
+348
+20 54 30 16 7 9
+*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodQuantity = int.Parse(Console.ReadLine());
+
+            int[] orders = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue< int> foodOrders = new Queue< int>(orders);
+
+            Console.WriteLine(foodOrders.Max());
+
+            while (foodOrders.Count > 0 && foodQuantity > 0)
+            {
+                int currentOrder = foodOrders.Peek();
+
+                if (foodQuantity - currentOrder >= 0)
+                {
+                    foodOrders.Dequeue();
+                    foodQuantity -= currentOrder;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (foodOrders.Count == 0)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            else
+            {
+                Console.WriteLine($"Orders left: {string.Join(" ", foodOrders)}");
+            }
+        }
+    }
+}
