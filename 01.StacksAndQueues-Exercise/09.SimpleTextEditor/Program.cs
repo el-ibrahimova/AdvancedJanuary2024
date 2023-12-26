283365185f8670505d1c3db50d05d33ad4cb6ed3
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-12-26 21:44:10 +0200

    create project

---
+++ 01.StacksAndQueues-Exercise/09.SimpleTextEditor/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace _09.SimpleTextEditor
+{
+/*
+1 HelloThere
+3 7
+2 2
+3 5
+4
+3 7
+4
+1 TestPassed
+3 5
+*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string text = string.Empty; // TODO: Replace with StringBuilder
+
+            Stack<string> states = new Stack<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] inputCommand = Console.ReadLine().Split();
+                
+                if (inputCommand[0] == "1")
+                {  
+                    states.Push(text);
+                    text += inputCommand[1];
+                }
+                else if (inputCommand[0] == "2")
+                {
+                   states.Push(text);
+                   int count = int.Parse(inputCommand[1]);
+                   text = text.Substring(0, text.Length - count);
+                }
+                else if (inputCommand[0] == "3")
+                {
+                    int index = int.Parse(inputCommand[1]);
+                    Console.WriteLine(text[index-1]);
+                }
+                else if (inputCommand[0] == "4")
+                {
+                    text = states.Pop();
+                }
+            }
+        }
+    }
+}
