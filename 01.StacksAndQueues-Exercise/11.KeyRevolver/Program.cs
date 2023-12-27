9484776492bf2873c7f6d78eb49812b50a2d3bb0
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-12-27 16:04:36 +0200

    create project

---
+++ 01.StacksAndQueues-Exercise/11.KeyRevolver/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace _11.KeyRevolver
+{
+/*
+50
+2
+11 10 5 11 10 20
+15 13 16
+1500
+*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int bulletPrice = int.Parse(Console.ReadLine());
+            int gunBarrelSize = int.Parse(Console.ReadLine());
+            int[] inputBullets = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] inputLocks = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int intelligence = int.Parse(Console.ReadLine());
+
+            Queue<int> locks = new Queue<int>(inputLocks);
+            Stack<int> bullets = new Stack<int>(inputBullets);
+
+            int tempGunBarrel= gunBarrelSize;
+            int firedBullets = 0;
+
+            while (locks.Count > 0 && bullets.Count > 0)
+            {
+                int currentBullet = bullets.Pop();
+                int currentLock = locks.Peek();
+
+                firedBullets++;
+
+                if (currentBullet <= currentLock)
+                {
+                    Console.WriteLine("Bang!");
+                    locks.Dequeue();
+                }
+                else
+                {
+                    Console.WriteLine("Ping!");
+                }
+
+                tempGunBarrel--;
+
+                if (tempGunBarrel == 0 && bullets.Count > 0)
+                {
+                    Console.WriteLine("Reloading!");
+                    tempGunBarrel = gunBarrelSize;
+                }
+            }
+
+            if (bullets.Count == 0 && locks.Count > 0)
+            {
+                Console.WriteLine($"Couldn't get through. Locks left: {locks.Count}");
+            }
+            else
+            {
+                Console.WriteLine($"{bullets.Count} bullets left. Earned ${intelligence - (firedBullets* bulletPrice)}");
+            }
+
+        }
+    }
+}
