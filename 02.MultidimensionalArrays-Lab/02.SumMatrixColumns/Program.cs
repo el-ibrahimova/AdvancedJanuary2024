aa2af05059bedaebbee10aae3ddcd3cfb8139eab
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-12-28 20:22:09 +0200

    create project

---
+++ 02.MultidimensionalArrays-Lab/02.SumMatrixColumns/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _02.SumMatrixColumns
+{
+/*
+3, 6
+7 1 3 3 2 1
+1 3 9 8 5 6
+4 6 7 9 1 0
+*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizes = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            int rows = sizes[0];
+            int cols = sizes[1];
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] rowValues = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = rowValues[col];
+                }
+            }
+
+            for (int col = 0; col < cols; col++)
+            {
+                int sum = 0;
+
+                for (int row = 0; row < rows; row++)
+                {
+                    sum += matrix[row, col];
+                }
+
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
