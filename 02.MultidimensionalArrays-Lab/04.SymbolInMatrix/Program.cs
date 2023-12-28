950499763a385d2c7417a3fb1df7cf90972fab61
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2023-12-28 20:22:46 +0200

    create project

---
+++ 02.MultidimensionalArrays-Lab/04.SymbolInMatrix/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _04.SymbolInMatrix
+{
+/*
+3
+ABC
+DEF
+X!@
+!
+*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int cols = rows;
+
+            char[,] matrix = new char[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                char[] rowValues = Console.ReadLine().ToArray();
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = rowValues[col];
+                }
+            }
+
+            char symbol = char.Parse(Console.ReadLine());
+            bool isFind = false;
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    if (matrix[row, col] == symbol)
+                    {
+                        Console.WriteLine($"({row}, {col})");
+                        isFind = true;
+                        return;
+                    }
+                }
+            }
+
+            if (isFind== false)
+            {
+                Console.WriteLine($"{symbol} does not occur in the matrix");
+            }
+        }
+    }
+}
