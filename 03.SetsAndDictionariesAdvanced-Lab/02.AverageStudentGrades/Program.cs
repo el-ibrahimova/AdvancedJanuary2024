034595440ef9a2ec0b59ad8e77dac8052a959316
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-01-04 20:48:16 +0200

    create project

---
+++ 03.SetsAndDictionariesAdvanced-Lab/02.AverageStudentGrades/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace _02.AverageStudentGrades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<decimal>> students = new Dictionary<string, List<decimal>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                string name = input[0];
+                decimal grade = decimal.Parse(input[1]);
+
+                if (!students.ContainsKey(name))
+                {
+                    students.Add(name, new List<decimal>());
+                }
+
+                students[name].Add(grade);
+            }
+
+            foreach (var student in students)
+            {
+                Console.Write($"{student.Key} -> ");
+
+                foreach (var grade in student.Value)
+                {
+                    Console.Write($"{grade:f2} ");
+                }
+
+                Console.WriteLine($"(avg: {student.Value.Average():f2})");
+            }
+        }
+    }
+}
