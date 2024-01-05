0c5ae22fd15216c42087a16e59d65867bde7c882
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-01-05 22:16:10 +0200

    create project

---
+++ 03.SetsAndDictionariesAdvanced-Lab/03.Largest3Numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _03.Largest3Numbers
+{
+    /*
+    10 30 15 20 50 5
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            //{10 30 15 20 50 5}
+
+            List<int> lagrest3numbers = numbers
+               .OrderByDescending(x=>x) // {50, 30, 20, 15, 10, 5}
+               .Take(3)  // {50, 30, 20}
+               .ToList();
+
+            Console.WriteLine(string.Join(" ", lagrest3numbers));
+
+            // По-кратко решение
+
+          // List<int> numbers = Console.ReadLine()
+          //     .Split()
+          //     .Select(int.Parse)
+          //     .OrderByDescending(x=>x)
+          //     .Take(3)
+          //     .ToList();
+          //   Console.WriteLine(string.Join(" ", numbers);
+        }
+    }
+}
