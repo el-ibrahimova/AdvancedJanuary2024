69bf68e9cfb828ec2d86d73039fb6b737bcc39ac
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-01-06 12:11:55 +0200

    create project

---
+++ 03.SetsAndDictionariesAdvanced-Lab/08.SoftUniParty/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace _08.SoftUniParty
+{
+    /*
+   7IK9Yo0h
+   9NoBUajQ
+   Ce8vwPmE
+   SVQXQCbc
+   tSzE5t0p
+   PARTY
+   9NoBUajQ
+   Ce8vwPmE
+   SVQXQCbc
+   END
+   */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> vipGuests = new HashSet<string>();
+            HashSet<string> regularGuests = new HashSet<string>();
+
+            bool partyStarted = false;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "END")
+                {
+                    break;
+                }
+
+                if (input == "PARTY")
+                {
+                    partyStarted = true;
+                }
+
+                if (partyStarted)
+                {
+                    if (char.IsDigit(input[0]))
+                    {
+                        vipGuests.Remove(input);
+                    }
+                    else
+                    {
+                        regularGuests.Remove(input);
+                    }
+                }
+                else
+                {
+                    if (char.IsDigit(input[0]))
+                    {
+                        vipGuests.Add(input);
+                    }
+                    else
+                    {
+                        regularGuests.Add(input);
+                    }
+                }
+            }
+
+            Console.WriteLine(vipGuests.Count + regularGuests.Count);
+
+            foreach (var guest in vipGuests)
+            {
+                Console.WriteLine(guest);
+            }
+
+            foreach (var guest in regularGuests)
+            {
+                Console.WriteLine(guest);
+            }
+
+
+        }
+
+    }
+}
