bda17ea4b0ee02a5d1976f90af5a1eed15e62169
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-01-06 12:11:24 +0200

    create project

---
+++ 03.SetsAndDictionariesAdvanced-Lab/06.RecordUniqueNames/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _06.RecordUniqueNames
+{
+/*
+8
+John
+Alex
+John
+Sam
+Alex
+Alice
+Peter
+Alex
+*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            HashSet<string> names = new HashSet<string>();
+
+            for (int i = 0; i < count; i++)
+            {
+                string name = Console.ReadLine();
+                names.Add(name); // добавя само име, което все още го няма
+            }
+
+            Console.WriteLine(string.Join("\n", names));
+        }
+    }
+}
