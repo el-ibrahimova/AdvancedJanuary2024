6b1e61686412fe308e8f379c32ccc69e4e2db599
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-01-07 12:38:41 +0200

    create project

---
+++ 03.SetsАndDictionariesAdvanced-Exercise/01.UniqueUsernames/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _01.UniqueUsernames
+{
+/*
+6
+John
+John
+John
+Peter
+John
+Boy1234
+*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int count = int.Parse(Console.ReadLine());
+
+           HashSet<string> names = new HashSet<string>();
+
+           for (int i = 0; i < count; i++)
+           {
+                string name = Console.ReadLine();
+                names.Add(name);
+           }
+
+           foreach (var name in names)
+           {
+               Console.WriteLine(name);
+           }
+        }
+    }
+}
