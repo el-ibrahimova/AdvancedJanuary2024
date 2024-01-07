a37b7ace92bfc5eeb7e5481108f945290ad97fe7
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-01-07 12:39:05 +0200

    create project

---
+++ 03.SetsАndDictionariesAdvanced-Exercise/03.PeriodicTable/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _03.PeriodicTable
+{
+ /*
+4
+Ce O
+Mo O Ce
+Ee
+Mo
+*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            SortedSet<string> periodicElements = new SortedSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] elements = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                periodicElements.UnionWith(elements);  // обединява елементите, като запазва тяхната уникалност, т.е. няма повтарящи се
+            }
+
+            Console.WriteLine(string.Join(" ", periodicElements));
+        }
+    }
+}
