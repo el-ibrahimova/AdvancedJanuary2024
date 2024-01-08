cafae6ff41abb00a51b291ce09924b359635d79e
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-01-08 21:19:06 +0200

    update

---
+++ 03.SetsАndDictionariesAdvanced-Exercise/05.CountSymbols/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _05.CountSymbols
+{
+/*
+SoftUni rocks
+*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            SortedDictionary<char, int> countChars = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (!countChars.ContainsKey(text[i]))
+                {
+                    countChars.Add(text[i], 0);
+                }
+                countChars[text[i]]++;
+            }
+
+            foreach (var chars in countChars)
+            {
+                Console.WriteLine($"{chars.Key}: {chars.Value} time/s");
+            }
+        }
+    }
+}
