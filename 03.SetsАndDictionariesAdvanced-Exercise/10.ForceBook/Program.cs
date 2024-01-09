86cd9d14da8e35921318765eb92b8503dc780b1d
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-01-09 19:55:43 +0200

    create project

---
+++ 03.SetsАndDictionariesAdvanced-Exercise/10.ForceBook/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Threading.Channels;
+
+namespace _10.ForceBook
+{
+    /*
+   Light | George
+   Dark | Peter
+   Lumpawaroo
+   */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, SortedSet<string>> sideUsers = new();
+
+            string command;
+            while ((command = Console.ReadLine()) != "Lumpawaroo")
+            {
+                if (command.Contains('|'))
+                {
+                    string[] tokens = command.Split(" | ", StringSplitOptions.RemoveEmptyEntries);
+                    string side = tokens[0];
+                    string user = tokens[1];
+
+                    if (!sideUsers.Values.Any(u => u.Contains(user))) // не се съдържа User в никоя от страните
+                    {
+                        if (!sideUsers.ContainsKey(side))
+                        {
+                            sideUsers.Add(side, new SortedSet<string>());
+                        }
+
+                        sideUsers[side].Add(user);
+                    }
+                }
+                else // contains " -> "
+                {
+                    string[] tokens = command.Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+                    string user = tokens[0];
+                    string side = tokens[1];
+
+                    foreach (var sideUser in sideUsers)
+                    {
+                        if (sideUser.Value.Contains(user))
+                        {
+                            sideUser.Value.Remove(user);
+                            break;
+                        }
+                    }
+
+                    if (!sideUsers.ContainsKey(side))
+                    {
+                        sideUsers.Add(side, new SortedSet<string>());
+                    }
+                    sideUsers[side].Add(user);
+
+                    Console.WriteLine($"{user} joins the {side} side!");
+                }
+            }
+
+            foreach (var sideUser in sideUsers.OrderByDescending(su => su.Value.Count))
+            {
+                if (sideUser.Value.Any())
+                {
+                    Console.WriteLine($"Side: {sideUser.Key}, Members: {sideUser.Value.Count}");
+
+                    foreach (var user in sideUser.Value)
+                    {
+                        Console.WriteLine($"! {user}");
+                    }
+                }
+            }
+        }
+    }
+}
