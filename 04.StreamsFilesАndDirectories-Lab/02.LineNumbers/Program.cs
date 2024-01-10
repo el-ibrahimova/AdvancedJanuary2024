959c22dea14ebfdd5f6c837c82278df7aa58ef06
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-01-10 19:05:28 +0200

    create ptoject

---
+++ 04.StreamsFilesАndDirectories-Lab/02.LineNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                string line = reader.ReadLine();
+                int counter = 1;
+
+                using (StreamWriter writer = new StreamWriter(outputFilePath))
+                {
+                    while (line != null)
+                    {
+                        writer.WriteLine($"{counter}. {line}");
+                        line = reader.ReadLine();
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}
