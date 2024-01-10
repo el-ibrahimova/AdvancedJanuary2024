aa11bb38f58bf464da245876282e751b967bc85c
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-01-10 19:05:06 +0200

    create project

---
+++ 04.StreamsFilesАndDirectories-Lab/01.OddLines/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace OddLines
+{
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath) {
+
+                using (StreamReader reader = new StreamReader(inputFilePath))
+                {
+                    int lineNumber = 0;
+
+                    using (StreamWriter writer = new StreamWriter(outputFilePath))
+                    {
+                        while (reader.EndOfStream == false) //докато не стигнем края на стрийма
+                        {
+                            string line = reader.ReadLine();
+
+                            if (lineNumber++ % 2 == 1)
+                            {
+                                writer.WriteLine(line);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
