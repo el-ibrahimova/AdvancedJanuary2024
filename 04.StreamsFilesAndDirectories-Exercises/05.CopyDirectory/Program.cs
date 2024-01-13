8d7fec00a0a00bca2a743a2733bae1de56fa3b54
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-01-13 09:32:50 +0200

    create project

---
+++ 04.StreamsFilesAndDirectories-Exercises/05.CopyDirectory/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace CopyDirectory
+{
+    using System;
+
+    public class CopyDirectory
+    {
+        static void Main()
+        {
+            string inputPath = @$"{Console.ReadLine()}";
+            string outputPath = @$"{Console.ReadLine()}";
+
+           // string inputPath = $@"D:\DirectoryToCopy";
+           // string outputPath = @$"D:\Output-Directory";
+
+
+            CopyAllFiles(inputPath, outputPath);
+        }
+
+        public static void CopyAllFiles(string inputPath, string outputPath)
+        {
+            if (Directory.Exists(outputPath))
+            {
+                Directory.Delete(outputPath, recursive: true);
+            }
+
+            Directory.CreateDirectory(outputPath);
+
+            string[] files = Directory.GetFiles(inputPath);
+
+            foreach (string file in files)
+            {
+                string fileName = Path.GetFileName(file);
+                string destination = Path.Combine(outputPath, fileName);
+                
+                 File.Copy(file, destination);
+            }
+        }
+    }
+}
