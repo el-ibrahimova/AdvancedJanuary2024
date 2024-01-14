c1fdaa418217c9c57b67a3a9734b9aee2aafa576
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-01-14 11:52:06 +0200

    create project

---
+++ 05.FunctionalProgramming-Lab/04.AddVAT/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _04.AddVAT
+{
+    /*
+     1.38, 2.56, 4.4
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, decimal> parser = s => decimal.Parse(s);
+           Func<decimal, decimal> vat = n => n * 1.2m;
+           
+           List< decimal> prices = Console.ReadLine()
+                .Split(", " , StringSplitOptions.RemoveEmptyEntries)
+                .Select(parser)
+                .Select(vat)
+                .ToList();
+
+           foreach (var price in prices)
+           {
+               Console.WriteLine($"{price:f2}");
+           }
+        }
+    }
+}
