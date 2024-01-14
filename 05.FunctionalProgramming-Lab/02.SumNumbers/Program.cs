b52ec999e577dbc219cc10e41c802fe6f49d0aac
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-01-14 11:51:33 +0200

    create project

---
+++ 05.FunctionalProgramming-Lab/02.SumNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _02.SumNumbers
+{
+    /*
+  4, 2, 1, 3, 5, 7, 1, 4, 2, 12
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int> parser = n => int.Parse(n); // Func за парсване на числата, прочетени от конзолата, към тип int
+            
+            int[] numbers = Console.ReadLine()
+                .Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(parser)
+                .ToArray();
+
+            Console.WriteLine(numbers.Length);
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
