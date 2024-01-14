3f109e921356f471cf986a9f78810f5e90030163
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-01-14 11:51:14 +0200

    create project

---
+++ 05.FunctionalProgramming-Lab/01.SortEvenNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _01.SortEvenNumbers
+{
+    /*
+   4, 2, 1, 3, 5, 7, 1, 4, 2, 12
+     */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> evenNumbers = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .Where(x=>x%2 == 0)
+                .OrderBy(x=>x)
+                .ToList();
+
+            Console.WriteLine(string.Join(", ", evenNumbers));
+        }
+    }
+}
