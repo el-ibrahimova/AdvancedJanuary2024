1819ba69ec819d9c3d5ad003519e2293b4236929
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-01-15 10:33:12 +0200

    create project

---
+++ 05.FunctionalProgramming-Exercise/02.KnightsOfHonor/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _02.KnightsOfHonor
+{
+    /*
+  Eathan Lucas Noah Arthur
+  */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);  
+           
+           Action<string, string[]> printNamesWithAddedTitle = (title,names) =>
+            {
+                foreach (var name in names)
+                {
+                    Console.WriteLine($"{title} {name}");
+                }
+            };
+           
+           printNamesWithAddedTitle("Sir", names);
+        }
+    }
+}
