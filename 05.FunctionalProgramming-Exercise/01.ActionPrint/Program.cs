aa58e1cc3096e3d843ef2872151ce422157b0645
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-01-15 10:33:01 +0200

    create project

---
+++ 05.FunctionalProgramming-Exercise/01.ActionPrint/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _01.ActionPrint
+{
+    /*
+  Lucas Noah Tea
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Action<string[]> print = names => Console.WriteLine(string.Join(Environment.NewLine, names));
+
+            print(names);
+            
+            //  string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            //  Action<string[]> print = (string[] names) =>
+            //  {
+            //      foreach (string s in names)
+            //       {
+            //           Console.WriteLine(s);
+            //       }
+            //  };
+            //  print(names);
+        }
+    }
+}
