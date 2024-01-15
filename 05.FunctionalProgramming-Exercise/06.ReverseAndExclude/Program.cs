97e79699f257de4e9ece737876814cc07ac1b58d
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-01-15 10:34:45 +0200

    create project

---
+++ 05.FunctionalProgramming-Exercise/06.ReverseAndExclude/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace _06.ReverseAndExclude
+{ 
+ /*
+ 1 2 3 4 5 6
+ 2
+ */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            int divider = int.Parse(Console.ReadLine());
+
+
+            Func<List<int>, List<int>> reverse = numbers =>
+            {
+                List<int> result = new List<int>();
+
+                for (int i = numbers.Count - 1; i >= 0; i--)
+                {
+                    result.Add(numbers[i]);
+                }
+                return result;
+            };
+
+            Func<List<int>, Predicate<int>, List<int>> exclude = (numbers, match) =>
+            {
+                List<int> result = new();
+
+                foreach (int number in numbers)
+                {
+                    if (!match(number))
+                    {
+                        result.Add(number);
+                    }
+                }
+                return result;
+            };
+            
+            Predicate<int> checkEven = number =>
+                number % divider == 0;
+
+            numbers = exclude(numbers, checkEven);
+            numbers = reverse(numbers);
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
