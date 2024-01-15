ce6d7b2374bba9fd3dd17e5f1ee1aaded5f8c3e8
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-01-15 10:33:42 +0200

    create project

---
+++ 05.FunctionalProgramming-Exercise/04.FindEvensOrOdds/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace _04.FindEvensOrOdds
+{
+    internal class Program
+    {
+        /*
+        1 10
+        odd
+           
+         */
+        static void Main(string[] args)
+        {
+            int[] ranges = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            string command = Console.ReadLine();
+
+            Func<int, int, List<int>> generateRange = (start, end) =>
+            {
+                List<int> range = new();
+
+                for (int i = start; i <= end; i++)
+                {
+                    range.Add(i);
+                }
+
+                return range;
+            };
+
+            Func<string, int, bool> EvenOddMatch = (condition, number) =>
+            {
+                if (condition == "even")
+                {
+                    return number % 2 == 0;
+                }
+                else // "odd"
+                {
+                    return number % 2 != 0;
+                }
+            };
+            
+            List<int> numbers = generateRange(ranges[0], ranges[1]);
+
+            foreach (var number in numbers)
+            {
+                if (EvenOddMatch(command, number))
+                {
+                    Console.Write($"{number} ");
+                }
+            }
+        }
+    }
+}
