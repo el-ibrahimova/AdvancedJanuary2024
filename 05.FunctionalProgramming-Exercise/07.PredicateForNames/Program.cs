810046bf388d85385489e8bc03fbe3a2764061f0
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-01-16 19:51:03 +0200

    create project

---
+++ 05.FunctionalProgramming-Exercise/07.PredicateForNames/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Channels;
+
+namespace _07.PredicateForNames
+{ 
+/*
+4
+Karl Anna Kris Yahto
+*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Action<string[], Predicate<string>> print = (names, match) =>
+            {
+                foreach (var name in names)
+                {
+                    if (match(name))
+                    {
+                        Console.WriteLine(name);
+                    }
+                }
+            };
+
+            print(names, n => n.Length <= length);
+        }
+    }
+}
