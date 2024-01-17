a6c4270e892acef033d208ea8f8c7f9d8cc8a3a7
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-01-17 21:07:43 +0200

    create project

---
+++ 06.Defining Classes-Lab/03.CarConstructors/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace CarManufacturer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Car car = new Car("BMW", "X3", 2006, 500, 300);
+            Car car2= new Car("BMW", "X3", 2006);
+            Car car3= new Car();
+
+            car.Drive(5);
+
+            Console.WriteLine(car.WhoAmI());
+        }
+    }
+}
