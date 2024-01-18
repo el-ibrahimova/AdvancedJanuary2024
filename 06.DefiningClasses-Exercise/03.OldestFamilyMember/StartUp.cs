1a330eaf0bf56aef45fbd3dc5f387c6b74ef223a
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-01-18 19:47:51 +0200

    create project

---
+++ 06.DefiningClasses-Exercise/03.OldestFamilyMember/StartUp.cs
@@ -0,0 +1,26 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Family family = new();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] tokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string name = tokens[0];
+                int age = int.Parse(tokens[1]);
+
+                Person person = new(name, age);
+                family.AddMember(person);
+            }
+
+            Person oldest = family.GetOldestMember();
+
+            Console.WriteLine($"{oldest.Name} {oldest.Age}");
+        }
+    }
+}
