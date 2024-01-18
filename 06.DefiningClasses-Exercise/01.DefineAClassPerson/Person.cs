bed8425ab988c6c65cbef29e2389189d901ccfc4
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-01-18 19:46:44 +0200

    create project

---
+++ 06.DefiningClasses-Exercise/01.DefineAClassPerson/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name  // записано по един начин
+        {
+            get =>this.name; 
+            set => this.name = value; 
+        }
+
+        public int Age // записано по друг начин
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                this.age = value;
+            }
+        }
+
+    }
+}
