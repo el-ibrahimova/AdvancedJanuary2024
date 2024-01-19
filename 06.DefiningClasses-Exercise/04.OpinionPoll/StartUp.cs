e31b882ecdc16e40a3ae838d73f8e207f5307d11
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-01-19 20:34:05 +0200

    create project

---
+++ 06.DefiningClasses-Exercise/04.OpinionPoll/StartUp.cs
@@ -0,0 +1,32 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Person> personOver30 = new List<Person>();
+            
+            int count = int.Parse(Console.ReadLine());
+            
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] tokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string name = tokens[0];
+                int age = int.Parse(tokens[1]);
+
+                Person person = new(name, age);
+
+                if (person.Age > 30)
+                {
+                    personOver30.Add(person);
+                }
+            }
+
+            foreach (var person in personOver30.OrderBy(p=>p.Name))
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+        }
+    }
+}
