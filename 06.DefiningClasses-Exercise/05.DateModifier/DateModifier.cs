7f681462afd3a8c735774b7a39bec51ec349a560
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-01-19 20:34:21 +0200

    create project

---
+++ 06.DefiningClasses-Exercise/05.DateModifier/DateModifier.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.DateModifier
+{
+    public static class DateModifier
+    {
+        public static int GetDifferenceInDays(string start, string end)
+        {
+           DateTime startDate =  DateTime.Parse(start);
+           DateTime endDate = DateTime.Parse(end);
+
+           TimeSpan difference = endDate -startDate;
+
+           int differenceInDays = difference.Days;
+
+            return Math.Abs(differenceInDays);
+
+        }
+    }
+}
---
+++ 06.DefiningClasses-Exercise/05.DateModifier/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _05.DateModifier
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           string start = Console.ReadLine();
+           string end = Console.ReadLine();
+            
+            int differenceInDays = DateModifier.GetDifferenceInDays(start, end);
+
+           Console.WriteLine(differenceInDays);
+        }
+
+    }
+}
