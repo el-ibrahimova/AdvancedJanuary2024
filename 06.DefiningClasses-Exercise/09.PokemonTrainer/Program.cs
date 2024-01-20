3669ab31ba31a36f6c910d6c928290cb1323d3cd
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-01-20 14:22:08 +0200

    create project

---
+++ 06.DefiningClasses-Exercise/09.PokemonTrainer/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace _09.PokemonTrainer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Trainer> listOfTrainers = new();
+            
+            string input;
+            while ((input = Console.ReadLine()) != "Tournament")
+            {
+                string[] info = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string trainerName = info[0];
+                string pokemonName = info[1];
+                string pokemonElement = info[2];
+                int pokemonHealth = int.Parse(info[3]);
+
+                if (!listOfTrainers.Any(x => x.Name == trainerName))
+                {
+                    listOfTrainers.Add(new Trainer(trainerName));
+                }
+
+                var currentTrainer = listOfTrainers.Find(t => t.Name == trainerName);
+                currentTrainer.Pokemons.Add(new Pokemon(pokemonName, pokemonElement, pokemonHealth));
+            }
+
+            string elements;
+            while ((elements = Console.ReadLine()) != "End")
+            {
+                foreach (Trainer trainer in listOfTrainers)
+                {
+                    if (trainer.Pokemons.Any(p => p.Element == elements))
+                    {
+                        trainer.Badges++;
+                    }
+                    else
+                    {
+                        for (int i = 0; i < trainer.Pokemons.Count; i++)
+                        {
+
+                            trainer.Pokemons[i].Health -= 10;
+                            if (trainer.Pokemons[i].Health <= 0)
+                            {
+                                trainer.Pokemons.RemoveAt(i);
+                                i--; // като изтрием елемент трябва да се върнем един ход назад, за да не пропуснем елемент от колекцията
+                            }
+                        }
+                    }
+                }
+            }
+
+            foreach (Trainer trainer in listOfTrainers.OrderByDescending(t => t.Badges))
+            {
+                Console.WriteLine($"{trainer.Name} {trainer.Badges} {trainer.Pokemons.Count}");
+            }
+        }
+    }
+}
---
+++ 06.DefiningClasses-Exercise/09.PokemonTrainer/Trainer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.PokemonTrainer
+{
+    public class Trainer
+    {
+        private string name;
+        private int badges;
+
+        public Trainer(string name)
+        {
+            Name = name;
+            Badges = 0;
+            Pokemons = new List<Pokemon>();
+        }
+
+        public string Name { get; set; }
+        public int Badges { get; set; }
+        public List<Pokemon> Pokemons { get; set; }
+        
+    }
+}
