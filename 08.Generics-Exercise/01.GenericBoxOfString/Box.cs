4b79f10e0ea096cdd0508afb4b1afc24b8f23a3d
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-01-23 15:52:50 +0200

    create project

---
+++ 08.Generics-Exercise/01.GenericBoxOfString/Box.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.GenericBoxOfString
+{
+    public class Box<T>
+    {
+        private List<T> items;
+
+        public Box()
+        {
+            items = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new();
+            
+            foreach (T item in items)
+            {
+                sb.AppendLine($"{typeof(T)}: {item.ToString()}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 08.Generics-Exercise/01.GenericBoxOfString/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _01.GenericBoxOfString
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<string> boxOfString = new();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string input = Console.ReadLine();
+                boxOfString.Add(input);
+            }
+
+            Console.WriteLine(boxOfString.ToString());
+        }
+    }
+}
