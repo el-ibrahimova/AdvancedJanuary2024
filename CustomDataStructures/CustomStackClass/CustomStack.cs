61c2d51c8f5080c92e6478130f5d70acd3a30726
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-01-23 15:51:34 +0200

    create project

---
+++ CustomDataStructures/CustomStackClass/CustomStack.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomStackClass
+{
+    public class CustomStack
+    {
+        private const int InitialCapacity = 4;
+        private int[] items;
+
+        public CustomStack()
+        {
+            items = new int[InitialCapacity];
+        }
+
+        public int Count { get; private set; }
+
+        public void Push(int item)
+        {
+            if (items.Length == Count)
+            {
+                Resize();
+            }
+
+            items[Count] = item;
+            Count++;
+        }
+
+        public int Pop()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("CustomStack is empty");
+            }
+
+            int removed = items[Count - 1];
+            items[Count - 1] = default; 
+            Count--;
+
+            // TODO shrink if needed
+            return removed;
+        }
+
+        public int Peek()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("CustomStack is empty");
+            }
+
+            return items[Count - 1];
+        }
+
+        public void ForEach(Action <int> action)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                int currentItem = items[i];
+                action(currentItem);
+            }
+
+            //for (int i = Count - 1; i >= 0; i--)
+            //{
+            //    int currentItem = items[i];
+            //    action(currentItem);
+            //}
+        }
+
+        private void Resize()
+        {
+            int[] copy = new int[items.Length * 2]; 
+
+            for (int i = 0; i < Count; i++)
+            {
+                copy[i] = items[i];
+            }
+            items = copy; 
+        }
+    }
+}
