883869d2ff2627af39b9bed94a64c52e4cfa4a30
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-01-25 20:54:22 +0200

    create project

---
+++ 08.Generics-Exercise/04.GenericSwapMethodInteger/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _04.GenericSwapMethodInteger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> items = new();
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                items.Add(input);
+            }
+
+            int[] indices = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int firstIndex = indices[0];
+            int secondIndex = indices[1];
+
+            Swap(firstIndex, secondIndex, items);
+
+            foreach (var item in items)
+            {
+                Console.WriteLine($"{item.GetType()}: {item}");
+            }
+        }
+
+        static void Swap<T>(int firstIndex, int secondIndex, List<T> items)
+        {
+            T temp = items[firstIndex];
+            items[firstIndex] = items[secondIndex];
+            items[secondIndex] = temp;
+
+            // друг вариант на обръщане на стойностите, когато променливите са инициализирани
+
+            // (items[secondIndex], items[firstIndex]) = (items[firstIndex], items[secondIndex]);
+        }
+    }
+}
