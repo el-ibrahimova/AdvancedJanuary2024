b987746347d96fd6badf8d67a926b411950dd22b
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-01-25 20:53:46 +0200

    create project

---
+++ 08.Generics-Exercise/02.GenericBoxOfInteger/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _02.GenericBoxOfInteger
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<int> boxOfInt = new();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                boxOfInt.Add(input);
+            }
+
+            Console.WriteLine(boxOfInt.ToString());
+        }
+    }
+}
