978b594be652024fe5808459d0b9482932c4508e
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-01-26 20:28:10 +0200

    create project

---
+++ 08.Generics-Lab/GenericScale/EqalityScale.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericScale
+{
+    public class EqalityScale<T> where T : IComparable<T> // наследява класа IComparable
+    {
+        private T left;
+        private T right;
+        public EqalityScale(T left, T right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+
+        public bool AreEqual()
+        {
+            return left.Equals(right);
+        }
+    }
+}
---
+++ 08.Generics-Lab/GenericScale/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            EqalityScale<int> scale = new EqalityScale<int>(5, 5);
+
+            Console.WriteLine(scale.AreEqual());
+        }
+    }
+}
