80e3ea2de8dca7d0c4062fcdca9da166b2300228
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-01-26 20:27:49 +0200

    create project

---
+++ 08.Generics-Lab/GenericArrayCreator/ArrayCreator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericArrayCreator
+{
+    public class ArrayCreator
+    {
+        public static T[] Create<T>(int length, T element) // ако този метод не е static, в StartUp ще трябва първо да направим инстанция на класа ArrayCreator, тогава да извикаме този метод. По този начин директно имаме достъп до него
+        {
+            T[]array = new T[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = element;
+            }
+
+            return array;
+        }
+    }
+}
---
+++ 08.Generics-Lab/GenericArrayCreator/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string [] list = ArrayCreator.Create(10, "Hello");
+
+            Console.WriteLine(String.Join(" ", list));
+        }
+    }
+}
