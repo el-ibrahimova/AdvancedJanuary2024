78e6ee898f27bdf837cdb9f1b64faf8da0f2d34e
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-01-27 15:34:07 +0200

    create project

---
+++ 08.Generics-Exercise/06.GenericCountMethodDouble/Box.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.GenericCountMethodDouble
+{
+    public class Box<T> where T : IComparable<T> // за да може да сравняваме стрингове, задаваме constraints where T: IComparable<T>
+    {
+        private List<T> items;
+
+        public Box()
+        {
+            items = new();
+        }
+
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+
+        public int CountMethod(T itemToCompare)
+        {
+            int count = 0;
+            foreach (T item in items)
+            {
+                if (item.CompareTo(itemToCompare) > 0) // ако не е по-голямо връща -1, т.е. ако връща положително число то стринга е по-голям. Ако върне 0, значи са еднакви
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
