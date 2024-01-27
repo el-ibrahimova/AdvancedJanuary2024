2b520d49ac3cf8157c8bd5a73614e5ee3201dcc6
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-01-27 15:33:48 +0200

    create project

---
+++ 08.Generics-Exercise/05.GenericCountMethodString/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _05.GenericCountMethodString
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           Box<string> box = new Box<string>();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string item = Console.ReadLine();
+                box.Add(item);
+            }
+
+            string itemToCompare = Console.ReadLine();
+
+            Console.WriteLine(box.CountMethod(itemToCompare));
+        }
+    }
+}
