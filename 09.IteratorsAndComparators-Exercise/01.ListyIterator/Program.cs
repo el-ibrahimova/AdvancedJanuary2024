53c82279142e4ae45fa1e59461d47654f237044e
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-01-30 21:24:55 +0200

    create project

---
+++ 09.IteratorsAndComparators-Exercise/01.ListyIterator/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ListyIteratorType
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> items = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Skip(1) // Пропусни първия елемент(знаем че първата команда е Create и я пропускаме, за да не я изписваме всеки път)
+                .ToList();
+
+            ListyIterator<string> listIterator = new(items); // изписваме и типа string, защото класа ни е Generic
+
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                switch (command)
+                {
+                    case "Move":
+                        Console.WriteLine(listIterator.Move());
+                        break;
+                    case "HasNext":
+                        Console.WriteLine(listIterator.HasNext());
+                        break;
+                    case "Print":
+                        try
+                        {
+                            listIterator.Print();
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+                }
+            }
+        }
+
+    }
+}
+
