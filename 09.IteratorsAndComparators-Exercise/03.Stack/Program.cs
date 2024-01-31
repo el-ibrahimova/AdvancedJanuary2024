d890e296631d8d94521ade099214d3104604b9ff
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-01-31 20:43:01 +0200

    create project

---
+++ 09.IteratorsAndComparators-Exercise/03.Stack/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace _03.Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] tokens = command
+                    .Split(new char [] {' ', ','}, StringSplitOptions.RemoveEmptyEntries);
+
+                string action = tokens[0];
+
+                if (action == "Push")
+                {
+                    int[] itemsToPush = tokens
+                        .Skip(1)
+                        .Select(int.Parse)
+                        .ToArray();
+
+                    foreach (int item in itemsToPush)
+                    {
+                        stack.Push(item);
+                    }
+                }
+                else // "Pop"
+                {
+                    try
+                    {
+                        stack.Pop();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+
+            //по условие колекцията трябва да се разпечата два пъти
+
+            foreach (int item in stack)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (int item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
