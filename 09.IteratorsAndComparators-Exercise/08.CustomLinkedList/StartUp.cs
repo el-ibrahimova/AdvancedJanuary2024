86c7f519ef422c72c230f265742b9493bab18c52
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-02-01 20:15:03 +0200

    create project

---
+++ 09.IteratorsAndComparators-Exercise/08.CustomLinkedList/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using CustomLinkedList;
+using System;
+
+namespace CustomLinkedList
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            DoubliLinkedList<int> list = new();
+
+            list.AddFirst(3);
+            list.AddFirst(2);
+            list.AddFirst(1);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            DoubliLinkedList<string> listString = new();
+
+            listString.AddFirst("some");
+            listString.AddFirst("random");
+            listString.AddFirst("string");
+
+            foreach (var item in listString)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
