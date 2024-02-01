6d5fec5452f88f031dda4262d9cbb91d650235ac
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-02-01 20:13:56 +0200

    create project

---
+++ 09.IteratorsAndComparators-Exercise/05.ComparingObjects/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.ComparingObjects
+{
+    public class Person : IComparable<Person>
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Town { get; set; }
+
+        public int CompareTo(Person other) //  връща -1, 0 или 1
+        {
+            int result = this.Name.CompareTo(other.Name);
+            
+            if (result != 0)
+            {
+                return result;
+            }
+
+            // => Имената са различни => срявняваме по години
+
+            result = this.Age.CompareTo(other.Age);
+
+            if (result != 0)
+            {
+                return result;
+            }
+            // => годините са различни => срявняваме по град
+
+            result =  this.Town.CompareTo(other.Town);
+
+            return result;
+        }
+    }
+}
---
+++ 09.IteratorsAndComparators-Exercise/05.ComparingObjects/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace _05.ComparingObjects
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new();
+
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] personProps = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                Person person = new()
+                {
+                    Name = personProps[0],
+                    Age = int.Parse(personProps[1]),
+                    Town = personProps[2]
+                };
+
+                people.Add(person);
+            }
+
+            int compareIndex = int.Parse(Console.ReadLine()) - 1; // по условие започваме да сравняваме от 1-ви индекс => за да не пропуснем човека на нулев индекс изваждаме -1
+
+            Person personToCompare = people[compareIndex];
+
+            int equalCount = 0;
+            int diffCount = 0;
+
+            foreach (var person in people)
+            {
+                if (person.CompareTo(personToCompare)==0) // еднакви са
+                {
+                    equalCount++;
+                }
+                else
+                {
+                    diffCount++;
+                }
+            }
+
+            if (equalCount == 1) // текущия се е сравнил 1 път със себе си => няма други равни на него => т.е. count трявба да е поне 2
+            {
+                Console.WriteLine("No matches");
+            }
+            else
+            {
+                Console.WriteLine($"{equalCount} {diffCount} {people.Count}");
+            }
+        }
+    }
+}
