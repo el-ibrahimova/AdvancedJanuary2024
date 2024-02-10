79d020d9b05ee828f2dd5149d3711f53f4874972
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-02-10 09:47:41 +0200

    create project

---
+++ ExamPreparation/01.BirthdayCelebration/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Numerics;
+
+namespace _01.BirthdayCelebration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> guest = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+            Stack<int> plate = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+            int wastedFood = 0;
+
+            while (guest.Any() && plate.Any())
+            {
+                int currentPlate = plate.Peek();
+                int currentGuest = guest.Peek();
+
+                int result = currentGuest - currentPlate;
+
+                if (result <= 0)
+                {
+                    wastedFood += result;
+                    plate.Pop();
+                    guest.Dequeue();
+                }
+                else //(result > 0)
+                {
+                    guest.Dequeue();
+                    plate.Pop();
+
+                    guest = new Queue<int>(guest.Reverse());
+                    guest.Enqueue(result);
+                    guest = new Queue<int>(guest.Reverse());
+                }
+            }
+
+            if (guest.Count > 0) 
+            {
+
+                Console.WriteLine($"Guests: {string.Join(" ", guest)}");
+            }
+
+            if (plate.Count > 0) 
+            {
+
+                Console.WriteLine($"Plates: {string.Join(" ", plate)}");
+            }
+
+            Console.WriteLine($"Wasted grams of food: {Math.Abs(wastedFood)}");
+        }
+    }
+}
