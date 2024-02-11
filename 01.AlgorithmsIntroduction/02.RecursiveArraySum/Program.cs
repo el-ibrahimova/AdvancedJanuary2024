863a12fc1171fd19e229b9c7529d5402c4a689d7
Elmaz Ibrahimova <el_ibrahimova@abv.bg>
2024-02-11 10:01:28 +0200

    create project

---
+++ 01.AlgorithmsIntroduction/02.RecursiveArraySum/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _02.RecursiveArraySum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          //  List<int> list = Enumerable.Range(0, 100).ToList();
+
+          List<int> list = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse)
+              .ToList();
+
+          Console.WriteLine(RecursiveSum(list, 0));
+          int RecursiveSum(List<int> list, int index)
+          {
+              if (list.Count - 1 == index)
+              {
+                  return list[index];
+              }
+
+              // pre-actions
+              //Console.WriteLine("Before recursion");
+              // recursion
+              int sumOfRestElements = RecursiveSum(list, index + 1);
+
+              //post-actions
+              //Console.WriteLine("After recursion");
+              int sumOfAllElements = list[index] + sumOfRestElements;
+
+              return sumOfAllElements;
+
+              // return list[index] + RecursiveSum(list, index+1);
+          }
+        }
+
+    }
+}
---
+++ 01.AlgorithmsIntroduction/02.RecursiveFactorial/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _02.RecursiveFactorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //5! = 5 * 4!
+            //4! = 4 * 3!(3 * 2 * 1)
+            //...
+            // 1! = 1
+
+            Console.WriteLine(NFactorial(int.Parse(Console.ReadLine())));
+            long NFactorial(int n)
+            {
+                if (n == 1)
+                {
+                    return 1;
+                }
+
+                long nMinusOneFactorial = NFactorial(n - 1);
+
+                return n * nMinusOneFactorial;
+            }
+
+
+            long NFactorialIterative(int n)
+            {
+                long result = 1;
+
+                for (int i = 1; i < n; i++)
+                {
+                    result *= i;
+                }
+
+                return result;
+            }
+        }
+    }
+}
